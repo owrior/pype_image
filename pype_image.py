5a383fadce77d7d87e5708fb5c15187cadc1b9c2
owrior <o.w.riordan@gmail.com>
2019-12-13 10:45:50 +0000

    Added functionality to get keywords.

--- pype_image.py
+++ pype_image.py
@@ -2,12 +2,13 @@ import subprocess
 import os
 import sys
 import warnings
+import json
 
 indicator = b"{ready}"
 block_size = 4096
 
 
-class ImagePype:
+class PypeImage:
     def __init__(self, executable="exiftool", meta_loc="iptc"):
         """meta_loc: iptc or xmp,
            executable: location of exiftool.exe (if in path exiftool fine)"""
@@ -15,10 +16,13 @@ class ImagePype:
         self.running = False
         if meta_loc is "iptc":
             self.meta_loc = "-iptc:Keywords"
+            self.getter_loc = "IPTC:Keywords"
         elif meta_loc is "xmp":
             self.meta_loc = "-xmp:Subject"
+            self.getter_loc = "XMP:Subject"
         else:
             self.meta_loc = "-iptc:Keywords"
+            self.getter_loc = "IPTC:Keywords"
 
     def start(self):
         if self.running:
@@ -53,12 +57,21 @@ class ImagePype:
         return output.strip()[:-len(indicator)]
 
     def addKeywords(self, metadata, fpath):
-        kws = [bytes("{0}+=\"{1}\"".format(self.meta_loc, kw.replace(" ", r" ")), 'utf8') for kw in metadata]
+        kws = [bytes("{0}+={1}".format(self.meta_loc, kw.replace(" ", r" ")), 'utf8') for kw in metadata]
         cmd = [b'-overwrite_original_in_place'] + kws + [fpath]
         self.execute(cmd)
 
     def clearKeywords(self, fpath):
-        cmd = [b'-overwrite_original_in_place', bytes('{0}='''.format(self.meta_loc), 'utf8')] + [fpath]
+        cmd = [b'-overwrite_original_in_place', bytes('{0}='''.format(self.meta_loc), 'utf8'), fpath]
         self.execute(cmd)
 
+    def getKeywords(self, fpath):
+        cmd = [b'-j', bytes('{0}'.format(self.meta_loc), 'utf8'), fpath]
+        out = self.execute(cmd)
+        info = json.loads(out)[0]
+        try:
+            return info[self.getter_loc]
+        except KeyError:
+            print(fpath, ' contains no ' + self.getter_loc + ' metadata.')
+            return ""
 
